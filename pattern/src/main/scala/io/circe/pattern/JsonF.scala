a48f57a06881ab51353fad681431f980389528ca
Travis Brown <travisrobertbrown@gmail.com>
2019-11-19 04:46:03 -0600

    Simplify constructor names

--- pattern/src/main/scala/io/circe/pattern/JsonF.scala
+++ pattern/src/main/scala/io/circe/pattern/JsonF.scala
@@ -16,23 +16,23 @@ sealed trait JsonF[+A] {
 }
 
 object JsonF {
-  final case object JNullF extends JsonF[Nothing] {
+  final case object NullF extends JsonF[Nothing] {
     def map[B](f: Nothing => B): JsonF[B] = this
   }
-  final case class JBooleanF(b: Boolean) extends JsonF[Nothing] {
+  final case class BooleanF(b: Boolean) extends JsonF[Nothing] {
     def map[B](f: Nothing => B): JsonF[B] = this
   }
-  final case class JNumberF(n: JsonNumber) extends JsonF[Nothing] {
+  final case class NumberF(n: JsonNumber) extends JsonF[Nothing] {
     def map[B](f: Nothing => B): JsonF[B] = this
   }
-  final case class JStringF(s: String) extends JsonF[Nothing] {
+  final case class StringF(s: String) extends JsonF[Nothing] {
     def map[B](f: Nothing => B): JsonF[B] = this
   }
-  final case class JArrayF[A](value: Vector[A]) extends JsonF[A] {
-    def map[B](f: A => B): JsonF[B] = JArrayF(value.map(f))
+  final case class ArrayF[A](value: Vector[A]) extends JsonF[A] {
+    def map[B](f: A => B): JsonF[B] = ArrayF(value.map(f))
   }
-  final case class JObjectF[A](fields: Vector[(String, A)]) extends JsonF[A] {
-    def map[B](f: A => B): JsonF[B] = JObjectF(fields.map { case (k, v) => (k, f(v)) })
+  final case class ObjectF[A](fields: Vector[(String, A)]) extends JsonF[A] {
+    def map[B](f: A => B): JsonF[B] = ObjectF(fields.map { case (k, v) => (k, f(v)) })
   }
 
   /**
@@ -46,12 +46,12 @@ object JsonF {
    * into Json. Can be used for catamorphisms.
    */
   def foldJson(jsonF: JsonF[Json]): Json = jsonF match {
-    case JNullF           => Json.Null
-    case JBooleanF(bool)  => Json.fromBoolean(bool)
-    case JStringF(string) => Json.fromString(string)
-    case JNumberF(value)  => Json.fromJsonNumber(value)
-    case JArrayF(vec)     => Json.fromValues(vec)
-    case JObjectF(fields) => Json.obj(fields: _*)
+    case NullF           => Json.Null
+    case BooleanF(bool)  => Json.fromBoolean(bool)
+    case StringF(string) => Json.fromString(string)
+    case NumberF(value)  => Json.fromJsonNumber(value)
+    case ArrayF(vec)     => Json.fromValues(vec)
+    case ObjectF(fields) => Json.obj(fields: _*)
   }
 
   private[this] type Field[A] = (String, A)
@@ -62,56 +62,56 @@ object JsonF {
     override def map[A, B](fa: JsonF[A])(f: A => B): JsonF[B] = fa.map(f)
 
     override def traverse[G[_], A, B](fa: JsonF[A])(f: A => G[B])(implicit G: Applicative[G]): G[JsonF[B]] = fa match {
-      case JNullF           => G.pure(JNullF)
-      case x @ JBooleanF(_) => G.pure(x)
-      case x @ JStringF(_)  => G.pure(x)
-      case x @ JNumberF(_)  => G.pure(x)
-      case JArrayF(vecA)    => G.map(catsStdInstancesForVector.traverse(vecA)(f))(vecB => JArrayF(vecB))
-      case JObjectF(fieldsA) =>
-        G.map(fieldInstance.traverse(fieldsA)(f))(fieldsB => JObjectF(fieldsB))
+      case NullF           => G.pure(NullF)
+      case x @ BooleanF(_) => G.pure(x)
+      case x @ StringF(_)  => G.pure(x)
+      case x @ NumberF(_)  => G.pure(x)
+      case ArrayF(vecA)    => G.map(catsStdInstancesForVector.traverse(vecA)(f))(vecB => ArrayF(vecB))
+      case ObjectF(fieldsA) =>
+        G.map(fieldInstance.traverse(fieldsA)(f))(fieldsB => ObjectF(fieldsB))
     }
 
     override def foldLeft[A, B](fa: JsonF[A], b: B)(f: (B, A) => B): B =
       fa match {
-        case JNullF        => b
-        case JBooleanF(_)  => b
-        case JStringF(_)   => b
-        case JNumberF(_)   => b
-        case JArrayF(vecA) => vecA.foldLeft(b)(f)
-        case JObjectF(fieldsA) =>
+        case NullF        => b
+        case BooleanF(_)  => b
+        case StringF(_)   => b
+        case NumberF(_)   => b
+        case ArrayF(vecA) => vecA.foldLeft(b)(f)
+        case ObjectF(fieldsA) =>
           fieldInstance.foldLeft(fieldsA, b)(f)
       }
 
     override def foldRight[A, B](fa: JsonF[A], lb: Eval[B])(f: (A, Eval[B]) => Eval[B]): Eval[B] =
       fa match {
-        case JNullF        => lb
-        case JBooleanF(_)  => lb
-        case JStringF(_)   => lb
-        case JNumberF(_)   => lb
-        case JArrayF(vecA) => catsStdInstancesForVector.foldRight(vecA, lb)(f)
-        case JObjectF(fieldsA) =>
+        case NullF        => lb
+        case BooleanF(_)  => lb
+        case StringF(_)   => lb
+        case NumberF(_)   => lb
+        case ArrayF(vecA) => catsStdInstancesForVector.foldRight(vecA, lb)(f)
+        case ObjectF(fieldsA) =>
           fieldInstance.foldRight(fieldsA, lb)(f)
       }
   }
 
   implicit def jsonFEqInstance[A: Eq]: Eq[JsonF[A]] = Eq.instance {
-    case (JNullF, JNullF)                       => true
-    case (JBooleanF(b1), JBooleanF(b2))         => b1 == b2
-    case (JStringF(s1), JStringF(s2))           => s1 == s2
-    case (JNumberF(jn1), JNumberF(jn2))         => jn1 == jn2
-    case (JArrayF(values1), JArrayF(values2))   => Eq[Vector[A]].eqv(values1, values2)
-    case (JObjectF(values1), JObjectF(values2)) => Eq[Vector[(String, A)]].eqv(values1, values2)
-    case _                                      => false
+    case (NullF, NullF)                       => true
+    case (BooleanF(b1), BooleanF(b2))         => b1 == b2
+    case (StringF(s1), StringF(s2))           => s1 == s2
+    case (NumberF(jn1), NumberF(jn2))         => jn1 == jn2
+    case (ArrayF(values1), ArrayF(values2))   => Eq[Vector[A]].eqv(values1, values2)
+    case (ObjectF(values1), ObjectF(values2)) => Eq[Vector[(String, A)]].eqv(values1, values2)
+    case _                                    => false
   }
 
   private[this] val unfolder: Json.Folder[JsonF[Json]] =
     new Json.Folder[JsonF[Json]] {
-      def onNull = JNullF
-      def onBoolean(value: Boolean) = JBooleanF(value)
-      def onNumber(value: JsonNumber) = JNumberF(value)
-      def onString(value: String) = JStringF(value)
-      def onArray(value: Vector[Json]) = JArrayF(value)
+      def onNull = NullF
+      def onBoolean(value: Boolean) = BooleanF(value)
+      def onNumber(value: JsonNumber) = NumberF(value)
+      def onString(value: String) = StringF(value)
+      def onArray(value: Vector[Json]) = ArrayF(value)
       def onObject(value: JsonObject) =
-        JObjectF(value.toVector)
+        ObjectF(value.toVector)
     }
 }
--- pattern/src/test/scala/io/circe/pattern/package.scala
+++ pattern/src/test/scala/io/circe/pattern/package.scala
@@ -1,6 +1,6 @@
 package io.circe
 
-import io.circe.pattern.JsonF.{ JArrayF, JBooleanF, JNullF, JNumberF, JObjectF, JStringF }
+import io.circe.pattern.JsonF.{ ArrayF, BooleanF, NullF, NumberF, ObjectF, StringF }
 import io.circe.testing.instances._
 import org.scalacheck.{ Arbitrary, Gen, Shrink }
 
@@ -8,23 +8,23 @@ package object pattern {
   implicit def arbitraryJsonF[A: Arbitrary]: Arbitrary[JsonF[A]] =
     Arbitrary(
       Gen.oneOf[JsonF[A]](
-        Arbitrary.arbitrary[Boolean].map(JBooleanF),
-        Arbitrary.arbitrary[JsonNumber].map(JNumberF),
-        Gen.const(JNullF),
-        Arbitrary.arbitrary[String].map(JStringF),
-        Arbitrary.arbitrary[Vector[(String, A)]].map(_.groupBy(_._1).mapValues(_.head._2).toVector).map(JObjectF.apply),
-        Arbitrary.arbitrary[Vector[A]].map(JArrayF.apply)
+        Arbitrary.arbitrary[Boolean].map(BooleanF),
+        Arbitrary.arbitrary[JsonNumber].map(NumberF),
+        Gen.const(NullF),
+        Arbitrary.arbitrary[String].map(StringF),
+        Arbitrary.arbitrary[Vector[(String, A)]].map(_.groupBy(_._1).mapValues(_.head._2).toVector).map(ObjectF.apply),
+        Arbitrary.arbitrary[Vector[A]].map(ArrayF.apply)
       )
     )
 
   implicit def shrinkJsonF[A](implicit A: Shrink[A]): Shrink[JsonF[A]] = Shrink {
-    case JsonF.JNullF       => Stream.empty
-    case JsonF.JBooleanF(_) => Stream.empty
-    case JsonF.JNumberF(n)  => shrinkJsonNumber.shrink(n).map(JsonF.JNumberF(_))
-    case JsonF.JStringF(s)  => Shrink.shrinkString.shrink(s).map(JsonF.JStringF(_))
-    case JsonF.JArrayF(values) =>
-      Shrink.shrinkContainer[Vector, A].shrink(values).map(JsonF.JArrayF(_))
-    case JsonF.JObjectF(fields) =>
-      Shrink.shrinkContainer[Vector, (String, A)].shrink(fields).map(JsonF.JObjectF(_))
+    case JsonF.NullF       => Stream.empty
+    case JsonF.BooleanF(_) => Stream.empty
+    case JsonF.NumberF(n)  => shrinkJsonNumber.shrink(n).map(JsonF.NumberF(_))
+    case JsonF.StringF(s)  => Shrink.shrinkString.shrink(s).map(JsonF.StringF(_))
+    case JsonF.ArrayF(values) =>
+      Shrink.shrinkContainer[Vector, A].shrink(values).map(JsonF.ArrayF(_))
+    case JsonF.ObjectF(fields) =>
+      Shrink.shrinkContainer[Vector, (String, A)].shrink(fields).map(JsonF.ObjectF(_))
   }
 }
