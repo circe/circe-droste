3fb48e1209869b8b3bec551a92898d2de0f8b2e3
Travis Brown <travisrobertbrown@gmail.com>
2020-02-18 05:43:40 -0600

    Miscellaneous updates

--- pattern/src/test/scala/io/circe/pattern/CirceSuite.scala
+++ pattern/src/test/scala/io/circe/pattern/CirceSuite.scala
@@ -2,12 +2,7 @@ package io.circe.pattern
 
 import io.circe.testing.ArbitraryInstances
 import org.scalatest.flatspec.AnyFlatSpec
-import org.scalatestplus.scalacheck.{ Checkers, ScalaCheckDrivenPropertyChecks }
-import org.typelevel.discipline.Laws
+import org.scalatestplus.scalacheck.ScalaCheckDrivenPropertyChecks
+import org.typelevel.discipline.scalatest.FlatSpecDiscipline
 
-trait CirceSuite extends AnyFlatSpec with ScalaCheckDrivenPropertyChecks with ArbitraryInstances {
-  def checkLaws(name: String, ruleSet: Laws#RuleSet): Unit = ruleSet.all.properties.zipWithIndex.foreach {
-    case ((id, prop), 0) => name should s"obey $id" in Checkers.check(prop)
-    case ((id, prop), _) => it should s"obey $id" in Checkers.check(prop)
-  }
-}
+trait CirceSuite extends AnyFlatSpec with FlatSpecDiscipline with ScalaCheckDrivenPropertyChecks with ArbitraryInstances
